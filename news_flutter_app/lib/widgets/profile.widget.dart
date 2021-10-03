72d904af4fc603c7677fad3e5f93d7dcd027da14
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-10-03 16:48:13 +0500

    Profile screen design completed

--- news_flutter_app/lib/widgets/profile.widget.dart
+++ news_flutter_app/lib/widgets/profile.widget.dart
@@ -12,21 +12,21 @@ class _ProfileState extends State<Profile> {
   final TextEditingController addressController = TextEditingController();
   final TextEditingController paymentDetailsController =
       TextEditingController();
+
+  saveProfile() {
+    print(nameController.value);
+    print(addressController.value);
+    print(paymentDetailsController.value);
+    nameController.clear();
+    addressController.clear();
+    paymentDetailsController.clear();
+    print("Profile saved");
+  }
+
   //
   Widget profileView() {
     return Column(
       children: <Widget>[
-        // Padding(
-        //   padding: EdgeInsets.fromLTRB(30, 50, 30, 30),
-        //   child: Row(
-        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        //     children: <Widget>[
-        //       Container(height: 50, width: 50 ,child: Icon(Icons.arrow_back_ios, size: 24,color: Colors.black54,), decoration: BoxDecoration(border: Border.all(color: Colors.black54), borderRadius: BorderRadius.all(Radius.circular(10))),),
-        //       Text('Profiles details', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-        //       Container(height: 24,width: 24)
-        //     ],
-        //   ),
-        // ),
         Padding(
           padding: const EdgeInsets.fromLTRB(0, 20, 0, 50),
           child: Stack(
@@ -75,25 +75,6 @@ class _ProfileState extends State<Profile> {
           ),
           child: Column(
             children: <Widget>[
-              // Padding(
-              //   padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-              //   child: Container(
-              //     height: 60,
-              //     child: Align(
-              //       alignment: Alignment.centerLeft,
-              //       child: Padding(
-              //         padding: const EdgeInsets.all(8.0),
-              //         child: Text(
-              //           'Name',
-              //           style: TextStyle(color: Colors.white70),
-              //         ),
-              //       ),
-              //     ),
-              //     decoration: BoxDecoration(
-              //         borderRadius: BorderRadius.all(Radius.circular(20)),
-              //         border: Border.all(width: 1.0, color: Colors.white70)),
-              //   ),
-              // ),
               Padding(
                 padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
                 child: TextFormField(
@@ -156,51 +137,11 @@ class _ProfileState extends State<Profile> {
                   ),
                 ),
               ),
-              // Padding(
-              //   padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-              //   child: Container(
-              //     height: 60,
-              //     child: Align(
-              //       alignment: Alignment.centerLeft,
-              //       child: Padding(
-              //         padding: const EdgeInsets.all(8.0),
-              //         child: Text(
-              //           'Email',
-              //           style: TextStyle(color: Colors.white70),
-              //         ),
-              //       ),
-              //     ),
-              //     decoration: BoxDecoration(
-              //         borderRadius: BorderRadius.all(Radius.circular(20)),
-              //         border: Border.all(width: 1.0, color: Colors.white70)),
-              //   ),
-              // ),
-              // Padding(
-              //   padding: const EdgeInsets.fromLTRB(20, 5, 20, 4),
-              //   child: Container(
-              //     height: 60,
-              //     child: Align(
-              //       alignment: Alignment.centerLeft,
-              //       child: Padding(
-              //         padding: const EdgeInsets.all(8.0),
-              //         child: Text(
-              //           'Type something about yourself',
-              //           style: TextStyle(color: Colors.white70),
-              //         ),
-              //       ),
-              //     ),
-              //     decoration: BoxDecoration(
-              //         borderRadius: BorderRadius.all(Radius.circular(20)),
-              //         border: Border.all(width: 1.0, color: Colors.white70)),
-              //   ),
-              // ),
               Expanded(
                 child: Align(
                   alignment: Alignment.bottomRight,
                   child: GestureDetector(
-                    onTap: () {
-                      print("Profile Saved!");
-                    },
+                    onTap: saveProfile,
                     child: Container(
                       height: 70,
                       width: 200,
