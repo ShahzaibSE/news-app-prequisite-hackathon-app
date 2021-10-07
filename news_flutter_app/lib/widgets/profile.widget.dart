1a92ef00a9ee19190fab25303368846599b7a381
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-10-07 20:59:51 +0500

    Updated

--- news_flutter_app/lib/widgets/profile.widget.dart
+++ news_flutter_app/lib/widgets/profile.widget.dart
@@ -279,6 +279,7 @@ class _ProfileState extends State<Profile> {
                 child: const Icon(
                   Icons.edit,
                   size: 26.0,
+                  color: Colors.white,
                 ),
               ),
             )
