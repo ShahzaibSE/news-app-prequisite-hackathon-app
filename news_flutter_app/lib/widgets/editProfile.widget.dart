2d293d3c1c4fd4fd81aa296527c1e51aafce670f
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-10-09 00:15:06 +0500

    Updated

--- news_flutter_app/lib/widgets/editProfile.widget.dart
+++ news_flutter_app/lib/widgets/editProfile.widget.dart
@@ -68,6 +68,9 @@ class _EditProfileState extends State<EditProfile> {
       firebase_storage.Reference ref =
           firebase_storage.FirebaseStorage.instance.ref("/$uid.png");
       final profile_pic_download_link = await ref.getDownloadURL();
+      print(name);
+      print(address);
+      print(payment);
       print("Path of the selected image");
       print(imagePath);
       print("Download link");
@@ -104,192 +107,184 @@ class _EditProfileState extends State<EditProfile> {
 
   //
   Widget profileView() {
-    return FutureBuilder(
-        future: getProfile(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 50),
-                  child: Stack(
-                    children: <Widget>[
-                      CircleAvatar(
-                        radius: 70,
-                        child: ClipOval(
-                          child: downloadLink == null
-                              ? Image.network(
-                                  snapshot.data['data']['imageUrl'],
-                                  height: 150,
-                                  width: 150,
-                                  fit: BoxFit.cover,
-                                )
-                              : Image.network(
-                                  downloadLink!,
-                                  height: 150,
-                                  width: 150,
-                                  fit: BoxFit.cover,
-                                ),
-                        ),
-                      ),
-                      Positioned(
-                          bottom: 1,
-                          right: 1,
-                          child: Container(
-                            height: 40,
-                            width: 40,
-                            child: GestureDetector(
-                              onTap: () {
-                                print("Upload your picture");
-                                pickImage();
-                              },
-                              child: Icon(
-                                Icons.add_a_photo,
-                                color: Colors.white,
-                              ),
+    // return FutureBuilder(
+    //     future: getProfile(),
+    //     builder: (BuildContext context, AsyncSnapshot snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.done) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20, 0, 50),
+          child: Stack(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 70,
+                child: ClipOval(
+                  child: imagePath == null
+                      ? Image.asset(
+                          'assets/enlightnment-app-logo.jpeg',
+                          height: 150,
+                          width: 150,
+                          fit: BoxFit.cover,
+                        )
+                      : downloadLink == null
+                          ? Image.asset(
+                              imagePath.toString(),
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.cover,
+                            )
+                          : Image.network(
+                              downloadLink!,
+                              height: 150,
+                              width: 150,
+                              fit: BoxFit.cover,
                             ),
-                            decoration: BoxDecoration(
-                                color: Colors.deepOrange,
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(20))),
-                          ))
-                    ],
-                  ),
-                ),
-                Expanded(
-                    child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(30),
-                        topRight: Radius.circular(30)),
-                    gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      colors: [
-                        Colors.black54,
-                        Color.fromRGBO(0, 41, 102, 1),
-                      ],
-                    ),
-                  ),
-                  child: Column(
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-                        child: TextFormField(
-                          enabled: isEditable,
-                          controller: nameController,
-                          key: const Key("name"),
-                          keyboardType: TextInputType.emailAddress,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            // hintText: "Name",
-                            hintText: snapshot.data['data']['name'].isEmpty
-                                ? "Name"
-                                : snapshot.data['data']['name'],
-                            hintStyle: const TextStyle(
-                              color: Colors.white,
-                            ),
-                            contentPadding: const EdgeInsets.fromLTRB(
-                                20.0, 10.0, 20.0, 10.0),
-                          ),
-                          style: const TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-                        child: TextFormField(
-                          enabled: isEditable,
-                          controller: addressController,
-                          key: const Key("address"),
-                          keyboardType: TextInputType.emailAddress,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            fillColor: Colors.white,
-                            // hintText: "Address",
-                            hintText: snapshot.data['data']['address'].isEmpty
-                                ? "Address"
-                                : snapshot.data['data']['address'],
-                            hintStyle: const TextStyle(
-                              color: Colors.white,
-                            ),
-                            contentPadding: const EdgeInsets.fromLTRB(
-                                20.0, 10.0, 20.0, 10.0),
-                          ),
-                          style: const TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
-                        child: TextFormField(
-                          enabled: isEditable,
-                          controller: paymentDetailsController,
-                          key: const Key("payment-card-number"),
-                          keyboardType: TextInputType.emailAddress,
-                          autofocus: true,
-                          decoration: InputDecoration(
-                            // hintText: "Debit/Card Number",
-                            hintText:
-                                snapshot.data['data']['card_number'].isEmpty
-                                    ? "Debit/Card Number"
-                                    : snapshot.data['data']['card_number'],
-                            hintStyle: const TextStyle(
-                              color: Colors.white,
-                            ),
-                            focusColor: Colors.white,
-                            contentPadding: const EdgeInsets.fromLTRB(
-                                20.0, 10.0, 20.0, 10.0),
-                          ),
-                          style: const TextStyle(
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: GestureDetector(
-                            onTap: saveProfile,
-                            child: Container(
-                              height: 70,
-                              width: 200,
-                              child: Align(
-                                child: Text(
-                                  'Save',
-                                  style: TextStyle(
-                                      color: Colors.white70, fontSize: 20),
-                                ),
-                              ),
-                              decoration: BoxDecoration(
-                                color: Colors.red,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(30),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ))
-              ],
-            );
-          } else {
-            return Container(
-              child: Center(
-                child: const AwesomeLoader(
-                  loaderType: AwesomeLoader.AwesomeLoader3,
-                  color: Colors.white,
                 ),
               ),
-              color: Colors.red,
-            );
-          }
-        });
+              Positioned(
+                  bottom: 1,
+                  right: 1,
+                  child: Container(
+                    height: 40,
+                    width: 40,
+                    child: GestureDetector(
+                      onTap: () {
+                        print("Upload your picture");
+                        pickImage();
+                      },
+                      child: Icon(
+                        Icons.add_a_photo,
+                        color: Colors.white,
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                  ))
+            ],
+          ),
+        ),
+        Expanded(
+            child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(30), topRight: Radius.circular(30)),
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [
+                Colors.black54,
+                Color.fromRGBO(0, 41, 102, 1),
+              ],
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
+                child: TextFormField(
+                  enabled: isEditable,
+                  controller: nameController,
+                  key: const Key("name"),
+                  keyboardType: TextInputType.emailAddress,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: "Name",
+                    // hintText: snapshot.data['data']['name'].isEmpty
+                    //     ? "Name"
+                    //     : snapshot.data['data']['name'],
+                    hintStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    contentPadding:
+                        const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
+                child: TextFormField(
+                  enabled: isEditable,
+                  controller: addressController,
+                  key: const Key("address"),
+                  keyboardType: TextInputType.emailAddress,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    hintText: "Address",
+                    // hintText: snapshot.data['data']['address'].isEmpty
+                    //     ? "Address"
+                    //     : snapshot.data['data']['address'],
+                    hintStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    contentPadding:
+                        const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 25, 20, 4),
+                child: TextFormField(
+                  enabled: isEditable,
+                  controller: paymentDetailsController,
+                  key: const Key("payment-card-number"),
+                  keyboardType: TextInputType.emailAddress,
+                  autofocus: true,
+                  decoration: InputDecoration(
+                    hintText: "Debit/Card Number",
+                    // hintText:
+                    //     snapshot.data['data']['card_number'].isEmpty
+                    //         ? "Debit/Card Number"
+                    //         : snapshot.data['data']['card_number'],
+                    hintStyle: const TextStyle(
+                      color: Colors.white,
+                    ),
+                    focusColor: Colors.white,
+                    contentPadding:
+                        const EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: GestureDetector(
+                    onTap: saveProfile,
+                    child: Container(
+                      height: 70,
+                      width: 200,
+                      child: Align(
+                        child: Text(
+                          'Save',
+                          style: TextStyle(color: Colors.white70, fontSize: 20),
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ))
+      ],
+    );
   }
 
   @override
