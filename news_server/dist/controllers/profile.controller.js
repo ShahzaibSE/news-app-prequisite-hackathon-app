990b3555e8bbffb6493a8acc7b1fc08c698693e5
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2021-10-11 23:37:47 +0500

    Updated

--- news_server/dist/controllers/profile.controller.js
+++ news_server/dist/controllers/profile.controller.js
@@ -30,23 +30,74 @@ const createProfile = async (req, res) => {
     const { uid, imageUrl, name, address, card_number } = req.body;
     //
     const existingProfile = await profileModel.findOne({ uid });
+    console.log("Existing profile");
+    console.log(existingProfile);
     if (existingProfile) {
-        let updatedProfile = await profileModel.updateOne({ uid }, { $set: { imageUrl, name, address, card_number } });
-        if (updatedProfile) {
-            res.status(200).send({
-                status: true,
-                resCode: 200,
-                message: "Profile updated successfully",
-                isError: false,
-            });
+        var profileObj = {};
+        if (imageUrl == "" && existingProfile["imageUrl"] != null) {
+            profileObj = { uid, imageUrl: existingProfile["imageUrl"] };
+        }
+        if (name == "" && existingProfile["name"] != null) {
+            console.log("Name not provided");
+            profileObj = { uid, imageUrl, name: existingProfile["name"] };
+        }
+        if (address == "" && existingProfile["address"] != null) {
+            profileObj = { uid, imageUrl, name, address: existingProfile["address"] };
+        }
+        if (card_number == "" && existingProfile["card_number"] != null) {
+            profileObj = {
+                uid,
+                imageUrl,
+                name,
+                address,
+                card_number: existingProfile["card_number"],
+            };
+        }
+        if (Object.keys(profileObj).length > 1) {
+            console.log("with few keys updated - Profile Obj");
+            console.log(profileObj);
+            let updatedProfile = await profileModel.updateOne({ uid }, 
+            // { $set: { imageUrl, name, address, card_number } }
+            { $set: { profileObj } });
+            if (updatedProfile) {
+                res.status(200).send({
+                    status: true,
+                    resCode: 200,
+                    message: "Profile updated successfully",
+                    isError: false,
+                });
+            }
+            else {
+                res.status(400).send({
+                    status: false,
+                    resCode: 400,
+                    message: "Error while updating profile",
+                    isError: true,
+                });
+            }
         }
         else {
-            res.status(400).send({
-                status: false,
-                resCode: 400,
-                message: "Error while updating profile",
-                isError: true
-            });
+            // console.log('Updating profile');
+            // console.log(profileObj);
+            let updatedProfile = await profileModel.updateOne({ uid }, 
+            // { $set: { imageUrl, name, address, card_number } }
+            { $set: { uid, imageUrl, name, address, card_number } });
+            if (updatedProfile) {
+                res.status(200).send({
+                    status: true,
+                    resCode: 200,
+                    message: "Profile updated successfully",
+                    isError: false,
+                });
+            }
+            else {
+                res.status(400).send({
+                    status: false,
+                    resCode: 400,
+                    message: "Error while updating profile",
+                    isError: true,
+                });
+            }
         }
     }
     else {
--- news_server/src/controllers/profile.controller.ts
+++ news_server/src/controllers/profile.controller.ts
@@ -30,25 +30,75 @@ export const createProfile = async (req: Request, res: Response) => {
   const { uid, imageUrl, name, address, card_number } = req.body;
   //
   const existingProfile = await profileModel.findOne({ uid });
+  console.log("Existing profile");
+  console.log(existingProfile);
   if (existingProfile) {
-    let updatedProfile = await profileModel.updateOne(
-      { uid },
-      { $set: { imageUrl, name, address, card_number } }
-    );
-    if (updatedProfile) {
-      res.status(200).send({
-        status: true,
-        resCode: 200,
-        message: "Profile updated successfully",
-        isError: false,
-      });
-    } else {
-        res.status(400).send({
-            status: false,
-            resCode: 400,
-            message: "Error while updating profile",
-            isError: true
+    var profileObj = {};
+    if (imageUrl == "" && existingProfile["imageUrl"] != null) {
+      profileObj = { uid, imageUrl: existingProfile["imageUrl"] };
+    }
+    if (name == "" && existingProfile["name"] != null) {
+      console.log("Name not provided");
+      profileObj = { uid, imageUrl, name: existingProfile["name"] };
+    }
+    if (address == "" && existingProfile["address"] != null) {
+      profileObj = { uid, imageUrl, name, address: existingProfile["address"] };
+    }
+    if (card_number == "" && existingProfile["card_number"] != null) {
+      profileObj = {
+        uid,
+        imageUrl,
+        name,
+        address,
+        card_number: existingProfile["card_number"],
+      };
+    }
+    if(Object.keys(profileObj).length > 1) {
+      console.log("with few keys updated - Profile Obj");
+      console.log(profileObj)
+      let updatedProfile = await profileModel.updateOne(
+        { uid },
+        // { $set: { imageUrl, name, address, card_number } }
+        { $set: { profileObj } }
+      );
+      if (updatedProfile) {
+        res.status(200).send({
+          status: true,
+          resCode: 200,
+          message: "Profile updated successfully",
+          isError: false,
         });
+      } else {
+        res.status(400).send({
+          status: false,
+          resCode: 400,
+          message: "Error while updating profile",
+          isError: true,
+        });
+      }
+    } else {
+      // console.log('Updating profile');
+      // console.log(profileObj);
+      let updatedProfile = await profileModel.updateOne(
+        { uid },
+        // { $set: { imageUrl, name, address, card_number } }
+        { $set: {uid, imageUrl, name, address, card_number} }
+      );
+      if (updatedProfile) {
+        res.status(200).send({
+          status: true,
+          resCode: 200,
+          message: "Profile updated successfully",
+          isError: false,
+        });
+      } else {
+        res.status(400).send({
+          status: false,
+          resCode: 400,
+          message: "Error while updating profile",
+          isError: true,
+        });
+      }
     }
   } else {
     let newProfile = new profileModel({
